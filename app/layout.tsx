d251dd72fad0ce342fe527ee6447e5fa93208ca5
bajudinasmalam <147392289+bajudinasmalam@users.noreply.github.com>
2024-03-20 12:18:34 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: '4 Pilihan Game Hoki188 yang sering JP di COBLOS4D',
   description: 'slot77, judi slot triofus, indoslot, jpslot, live hongkong, data hk, live draw hk tercepat, slot online, judi338, hoki188, situs deposit dana 10k, slot anti rungkat, slot pragmatic, infini88, bomslot, hoki55, slot dana gratis, dana saldo gratis, slot dana tanpa potongan, keluaran sgp hari ini, keluaran sdy hari ini, keluaran hk hari ini, prediksi togel apa besok, ',
+  verification: {
+    google: '17I5CCSLClHuCsfUPROcbZIwIl4KRnyxqtG1OYEtYMk',
+  },
 }
-
 export default function RootLayout({
   children,
 }: {
